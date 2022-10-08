e78949acab0fa665b6018e00097e1bc42b377b45
Jimmy <jimmyquadros@hotmail.com>
2022-10-08 15:24:51 -0400

    script refactor, minimax bug

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ const gameBoard = (() => {
     let inPlay = false;
     let thisTurn = players[0];
     let notThisTurn = players[1];
+    let winIndex = null;
 
     // Getters and setter
     const getInPlay = () => inPlay;
@@ -21,62 +22,77 @@ const gameBoard = (() => {
     const setInPlay = () => inPlay = !inPlay;
     const setState = (index, value) => state[index] = value;
 
-    const resetState = () => state.fill(' ');
+    const resetState = () => {
+        state.fill(' ');
+        winIndex = null;
+    }
     const togglePlayer = () => {
         thisTurn = (thisTurn == players[0]) ? players[1] : players[0];
         notThisTurn = (notThisTurn == players[1]) ? players[0] : players[1];
     }
 
     // End state checkers
-    const tieState = () => !state.includes(' ');
+    const tieState = () => {
+        (!state.includes(' ') && winIndex == null);
+    }
     const winState = (index) => {
         if (index % 2 == 0 && state[4] != ' ') {
             if (state[0] == state[4] && 
-                state[0] == state[8]) { return [0, 4, 8] } 
+                state[0] == state[8]) { 
+                    winIndex = [0, 4, 8];
+                    return true;
+                } 
             if (state[2] == state[4] && 
-                state[2] == state[6]) { return [2, 4, 6] }
+                state[2] == state[6]) { 
+                    winIndex = [2, 4, 6];
+                    return true;
+                }
         }
         const row = Math.floor(index / 3) * 3;
         if (state[row] == state[row + 1] && 
-            state[row] == state[row + 2]) { return [row, row + 1, row + 2] }
+            state[row] == state[row + 2]) {
+                winIndex = [row, row + 1, row + 2];
+                return true;
+            }
         const column = index % 3;
         if (state[column] == state[column + 3] && 
-            state[column] == state[column + 6]) { return [column, column + 3, column + 6] }
-        return false;
-    }
-    const isWin = (index) => {
-        const win = winState(index);
-        if (win) { return win }
-        if (tieState()) { return 'tie' }
+            state[column] == state[column + 6]) {
+                winIndex = [column, column + 3, column + 6];
+                return true;}
         return false;
     }
 
-    const miniMax = (depth, isMax, win) => {
-        if (win && win == 'tie') {
+    const getWin = () => winIndex;
+    
+
+    const miniMax = (depth, isMaxTurn, win) => {
+        if (tieState()) {
             return 0;
-        } else if (win && !isMax) {
+        } else if (win && !isMaxTurn) {
+            winIndex = null;
             return 10 - depth;
-        } else if (win && isMax) {
+        } else if (win && isMaxTurn) {
+            winIndex = null;
             return -10 + depth;
         }
 
-        if (isMax) {
+        if (isMaxTurn) {
             let best = -11;
             for (let i = 0; i < state.length; i++) {
                 if (getState(i) == ' ') {
                     setState(i, getPlayer());
-                    best = Math.max(best, miniMax(depth + 1, !isMax, isWin(i)));
+                    best = Math.max(best, miniMax(depth + 1, !isMaxTurn, winState(i)));
                     setState(i, ' ');
                 }
             }
             return best;
         }
-        if (!isMax) {
+        if (!isMaxTurn) {
             let best = 11;
             for (let i = 0; i < state.length; i++) {
                 if (getState(i) == ' ') {
                     setState(i, getOpponent());
-                    best = Math.min(best, miniMax(depth + 1, !isMax, isWin(i)));
+                    best = Math.min(best, miniMax(depth + 1, !isMaxTurn, winState(i)));
                     setState(i, ' ');
                 }
             }
@@ -91,10 +107,9 @@ const gameBoard = (() => {
         for (let i = 0; i < state.length; i++) {
             if (getState(i) == ' ') {
                 setState(i, getPlayer());
-                const max = miniMax(0, false, isWin(i));
+                const max = miniMax(0, false, winState(i));
                 if (max > bestMove[1]) {
                     bestMove = [ i, max ];
-                    // console.log(bestMove);
                 }
                 setState(i, ' ');
             }
@@ -112,7 +127,7 @@ const gameBoard = (() => {
         getOpponent,
         getPlayer,
         getState,
-        isWin,
+        getWin,
         printState,
         resetState,
         setInPlay,
@@ -157,23 +172,22 @@ const displayController = (() => {
     }
 
     // fades out the display area after game, highlighting the winning move
-    const disableDisplay = (win) => {
+    const disableDisplay = () => {
         document.getElementById('board').classList.add('no-play');
         for (const block of document.getElementById('board').children) {
-            if (win == 'tie' || !win.includes(Number(block.id.slice(-1)))) {
+            if ((gameBoard.tieState()) || !gameBoard.getWin().includes(Number(block.id.slice(-1)))) {
                 block.classList.add('no-block');
             }
         }
     }
 
     // confirms if the game is over and sets the board accordingly
-    const checkWin = (win) => {
-        if (!win) { return }
+    const setWin = () => {
+        disableDisplay();
         const winButton = makeButton(
-            (win == 'tie') ? `NO WINNER` : `${gameBoard.getPlayer()} WINS`);
+            (gameBoard.tieState()) ? `NO WINNER` : `${gameBoard.getPlayer()} WINS`);
         const playAgainButton = makeButton('PLAY AGAIN?');
         bindNewGame(playAgainButton, false);
-        disableDisplay(win);
         document.getElementById('top').appendChild(winButton);
         document.getElementById('bottom').appendChild(playAgainButton);
         gameBoard.setInPlay();
@@ -188,6 +202,25 @@ const displayController = (() => {
         bindNewGame(p2Button, false);
         document.getElementById('bottom').appendChild(p2Button);
     })();
+
+    const setBlock = (block, toSet) => {
+        block.innerHTML = toSet;
+        block.className = '';
+    }
+
+    const soloPlay = () => {
+        const move = gameBoard.cpuTurn();
+        setBlock(document.querySelector(
+            `#board :nth-child(${move + 1})`), 
+            gameBoard.getPlayer());
+        console.log('them: ' + gameBoard.printState());
+        return move;  
+    }
+
+    const isGameOver = (index) => {
+        if (gameBoard.winState(index) || gameBoard.tieState()) return true;
+        return false;
+    }
     
     // Bindings for each square on the gameboard
     for (const block of document.getElementById('board').children) {
@@ -195,36 +228,27 @@ const displayController = (() => {
 
         // Highlights a square when hovered over
         block.addEventListener('pointerenter', (e) => {
-            if (gameBoard.getState(index) == ' ' && gameBoard.getInPlay()) {
-                e.target.innerHTML = gameBoard.getPlayer();
-                e.target.classList.add('hover');
-            }
+            if (gameBoard.getState(index) != ' ' || !gameBoard.getInPlay()) { return; }
+            setBlock(e.target, gameBoard.getPlayer());
+            e.target.classList.add('hover');
         });
 
         // Clear square highlighting
         block.addEventListener('pointerleave', (e) => {
-            if (gameBoard.getState(index) == ' ' && gameBoard.getInPlay()) {
-                e.target.innerHTML = '';
-                e.target.classList.remove('hover');
-            }
+            if (gameBoard.getState(index) != ' ' || !gameBoard.getInPlay()) { return; }
+            setBlock(e.target, '');
         });
 
         // Sets all actions taken on a click
         block.addEventListener('click', (e) => {
-            if (gameBoard.getState(index) == ' ' && gameBoard.getInPlay()) {           
-                e.target.innerHTML = gameBoard.getPlayer();
-                e.target.classList.remove('hover');
-                gameBoard.setState(index, gameBoard.getPlayer());
-                const isWin = gameBoard.isWin(index);
-                if (!isWin && onePlayer) {
-                    const move = gameBoard.cpuTurn();
-                    const block = document.querySelector(`#board :nth-child(${move + 1})`);
-                    block.innerHTML = gameBoard.getPlayer();
-                    checkWin(gameBoard.isWin(move));
-                }
-                checkWin(isWin);
-                gameBoard.togglePlayer();
-            }
+            if (gameBoard.getState(index) != ' ' || !gameBoard.getInPlay()) { return; }
+            console.log('you: ' + gameBoard.printState());
+            e.target.classList.remove('hover');
+            gameBoard.setState(index, gameBoard.getPlayer());
+            if (isGameOver(index) || (onePlayer && isGameOver(soloPlay()))) { 
+                setWin(); 
+            } 
+            gameBoard.togglePlayer();            
         })
     }
 })();
